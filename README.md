4c2ea0763030d97d1fa564bf3d7a028f9c678203
Ryszard Knop <dragoon@dragonic.eu>
2021-09-17 22:12:51 +0200

    Update notes and pics after the conference

--- README.md
+++ README.md
@@ -11,3 +11,24 @@ To play with this, you'll need:
 You can learn more about the conference on [its homepage](https://xdc2021.x.org/).
 
 Please steal. :)
+
+## Details
+
+In general, the setup is not that far away from what was done in the previous year, see:
+
+- Last year's [ivyl's blog](https://blog.hiler.eu/xdc-2020/)
+- Last year's [mupuf's blog](https://mupuf.org/blog/2020/09/28/3-ways-to-host-a-conference-using-jitsi/)
+- Music: Jeremy Blake - Through The Crystal ([from YouTube Audio Library](https://www.youtube.com/audiolibrary) and not redistributable)
+- Slides for [How we did XDC 2021](https://indico.freedesktop.org/event/1/contributions/46/attachments/29/39/TechStuff.pdf) for infra/services overview
+- Lock down YouTube comments - there's a "force review" mode on all new comments available. I missed that on day 1 and regretted it.
+- OBS scenes in use are in this repo. We had two OBS instances, primary running talks and backup running workshops.
+- There was no 3rd OBS setup to switch between those - instead, each streamed to a Nginx RTMP box, which forwarded the stream to YouTube and media.ccc.de (thank you!) - Nginx config is in the repo.
+- For media.ccc.de, you need a [config](https://github.com/voc/streaming-website/tree/master/configs/conferences/xdc2021) and the fahrplan XML from [this converter](https://github.com/voc/voctosched/pull/45)
+- We hosted Jitsi and the RTMP gateway on Hetzner with their CPX31 instances. It cost us ~5 EUR total for all 3 days (and maybe 1EUR extra for testing before the conf).
+
+
+## Nice to haves
+
+- A single person streaming for 6-7 hours straight is a bit much - having someone to switch halfway through the day would be nice but wasn't so easy to coordinate online. (The 3rd OBS setup from the last year would've helped here :D)
+- I wanted an animated background live from Godot, but running that caused some frame drops in OBS when also running Jitsi - so back to the static background we went.
+- Automate everything - we had a handy switcher to quickly swap the next talk/presenter name, but having that automated so that it'd switch automatically would help, too. OBS has built-in scripting and we used it for powerline-like details - it should be possible to fully automate title changes too.
--- Text/Switcher.py
+++ Text/Switcher.py
@@ -8,22 +8,24 @@ day1 = [
     { "title": "The Occult and the Apple GPU", "speaker": "Alyssa Rosenzweig, Collabora" },
     { "title": "ChromeOS + freedreno update", "speaker": "Rob Clark, Google" },
     { "title": "SSA-based Register Allocation\nfor GPU Architectures", "speaker": "Connor Abbott and Daniel Schürmann, Valve" },
-    { "title": "etnativ: status update", "speaker": "Christian Gmeiner" },
-    { "title": "Fast Checkpoint Restore\nfor AMD GPUs with CRIU", "speaker": "Rajneesh Bhardwaj and Felix Kuehling, AMD" },
+    { "title": "etnaviv: status update", "speaker": "Christian Gmeiner" },
+    { "title": "Fast Checkpoint Restore\nfor AMD GPUs with CRIU", "speaker": "Rajneesh Bhardwaj, Felix Kuehling and David Yat Sin, AMD" },
     { "title": "Emulating Virtual Hardware in VKMS", "speaker": "Sumera Priyadarsini" },
-    { "title": "Lightning Talks", "speaker": "Ricardo Garcia, Alyssa Rosenzweig and Roman Gilg" }
+    { "title": "Lightning Talks", "speaker": "Arthur Rasmusson, Alyssa Rosenzweig, Roman Gilg\nRicardo Garcia and Ethan Lee" },
+    { "title": "Non! End of Day 1!", "speaker": "See you tomorrow!" },
 ]
 
 day2 = [
     { "title": "Opening Session", "speaker": "Radosław Szwichtenberg, Intel" },
     { "title": "Addressing Wayland robustness", "speaker": "David Edmundson, KDE" },
-    { "title": "Compiling Vulkan shaders in the browser:\nA tale of control flow graphs and WebAssembly", "speaker": "Tony Wasserka" },
+    { "title": "Compiling Vulkan shaders in the browser:\nA tale of control flow graphs and WebAssembly", "speaker": "Tony Wasserka, Valve" },
     { "title": "Dissecting and fixing Vulkan rendering\nissues in drivers with RenderDoc", "speaker": "Danylo Piliaiev, Igalia" },
     { "title": "Ray-tracing in Vulkan, Part 2: Implementation", "speaker": "Jason Ekstrand, Intel" },
-    { "title": "Enabling Level Zero Sysman APIs for\ntool developers to control the GPUs", "speaker": "Saikishore Konda, Ravindra Babu Ganapathi,\nJitendra Sharma and T J Vivek Vilvaraj, Intel" },
-    { "title": "Redefining the Future of\nAccelerator Computing with Level Zero", "speaker": "Jaime Arteaga, Ravindra Babu Ganapathi, Aravind Gopalakrishnan,\nMichal Mrozek, Brandon Fliflet and Ben Ashbaugh, Intel" },
+    { "title": "KWinFT in 2021: Latest Development, Next Steps", "speaker": "Roman Gilg" },
+    { "title": "Enabling Level Zero Sysman APIs for\ntool developers to control the GPUs", "speaker": "Saikishore Konda and Ravindra Babu Ganapathi, Intel" },
+    { "title": "Redefining the Future of\nAccelerator Computing with Level Zero", "speaker": "Jaime Arteaga, Michal Mrozek, et al, Intel" },
     { "title": "X.Org Security", "speaker": "Matthieu Herrb" },
-    { "title": "X.Org Foundation Board of Directors Meeting", "speaker": "Emma Anholt, Samuel Iglesias Gonsálvez, Mark Filion, Manasi D Navare,\nKeith Packard, Lyude Paul, Daniel Vetter and Harry Wentland" }
+    { "title": "Null! End of Day 2!", "speaker": "(Board meeting is not streamed.)\nSee you tomorrow!" },
 ]
 
 day3 = [
@@ -32,14 +34,23 @@ day3 = [
     { "title": "KWinFT's wlroots backend", "speaker": "Roman Gilg" },
     { "title": "TTM conversion in i915", "speaker": "Thomas Hellstrom, Intel" },
     { "title": "Status of freedesktop.org GitLab/cloud hosting", "speaker": "Benjamin Tissoires, Red Hat" },
-    { "title": "Making bare-metal testing accessible\nto every developer", "speaker": "Martin Peres, X.Org" },
+    { "title": "Making bare-metal testing accessible\nto every developer", "speaker": "Martin Roukala, X.Org/Valve" },
     { "title": "A new CPU performance scaling proposal\nfor tuning VKD3D-Proton", "speaker": "Ray Huang" },
     { "title": "Video decoding in Vulkan: A brief overview of the\nVK_KHR_video_queue & VK_KHR_video_decode APIs", "speaker": "Victor Manuel Jáquez Leal, Igalia" },
     { "title": "State of the X.Org", "speaker": "Lyude Paul, Red Hat" },
-    { "title": "Closing Session", "speaker": "Everyone!" },
+    { "title": "Lightning Talks", "speaker": "Everyone!" },
+    { "title": "Closing Session", "speaker": "Radosław Szwichtenberg" },
 ]
 
-use_day = day1
+workshops = [
+    { "title": "Coordinating the CI efforts for Linux + userspace", "speaker": "Martin Peres, X.Org / Valve contractor" },
+    { "title": "SSA-based Register Allocation", "speaker": "Connor Abbott and Daniel Schürmann, Valve" },
+    { "title": "Hostile Multi-Tenancy on a Single Commodity GPU: Can it be secure?", "speaker": "Demi Obenour" },
+    { "title": "X.Org security BoF", "speaker": "Matthieu Herrb" },
+]
+
+use_day = day3
+
 
 if len(sys.argv) == 1:
     new_line = '\n'
